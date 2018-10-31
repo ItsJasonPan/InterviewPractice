fcd3ca0ddb6f8fc19dd27c9c271911b67962f1c3
ItsJasonPan <yingjiap@andrew.cmu.edu>
2018-10-31 15:16:08 -0400

    add a practice problem

---
+++ practice questions/Extra_long_factorials.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    pass
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
